66f7454628be652637cbf529099a435e42d47f88
Chrisian Goltz <christiangoltz@hotmail.com>
2017-01-29 14:34:02 +0100

    working in progress

--- src/main/java/com/chris/training/controller/GifController.java
+++ src/main/java/com/chris/training/controller/GifController.java
@@ -6,17 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class GifController {
 
     @Autowired
     private GifRepository gifRepository;
-
-    //@GetMapping("/")
-    //public String listGifs() {
-    //    return "home";
-    //}
 
     @GetMapping("/")
     public String listGifs(ModelMap modelMap){
@@ -30,6 +26,18 @@ public class GifController {
         Gif gif = gifRepository.findByName("android-explosion");
         // model map to hash mapa
         modelMap.addAttribute("gif", gif);
+        return "gif-details";
+    }
+
+    @GetMapping("/favorites")
+    public String favorites(ModelMap modelMap){
+        modelMap.addAttribute("gifs", gifRepository.getFavorites());
+        return "favorites";
+    }
+
+    @GetMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap){
+        modelMap.put("gif", gifRepository.findByName(name));
         return "gif-details";
     }
 
--- src/main/java/com/chris/training/repository/GifRepository.java
+++ src/main/java/com/chris/training/repository/GifRepository.java
@@ -3,6 +3,7 @@ package com.chris.training.repository;
 import com.chris.training.model.Gif;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,15 +11,15 @@ import java.util.List;
 public class GifRepository {
 
     private static List<Gif> All_GIFS = Arrays.asList(new Gif("android-explosion", "michalos", false),
-            new Gif("cowboy-coder", "Chris", false),
+            new Gif("cowboy-coder", "Chris", true),
             new Gif("ben-and-mike", "Chris", false),
             new Gif("book-dominos", "Magda", false),
             new Gif("compiler-bot", "Magda", false),
-            new Gif("infinite-andrew", "Magda", false));
+            new Gif("infinite-andrew", "Magda", true));
 
-    public Gif findByName(String name){
-        for (Gif gif: All_GIFS){
-            if(gif.getName().equals(name))
+    public Gif findByName(String name) {
+        for (Gif gif : All_GIFS) {
+            if (gif.getName().equals(name))
                 return gif;
             else
                 return null;
@@ -26,7 +27,16 @@ public class GifRepository {
         return null;
     }
 
-    public List<Gif> getAllGifs(){
+    public List<Gif> getAllGifs() {
         return All_GIFS;
+    }
+
+    public List<Gif> getFavorites() {
+        List<Gif> favorites = new ArrayList<>();
+        for (Gif gif : All_GIFS) {
+            if (gif.isFavorite())
+                favorites.add(gif);
+        }
+        return favorites;
     }
 }
